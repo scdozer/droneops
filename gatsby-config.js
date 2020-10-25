90148bed51b772fa478d0d0d025ad466f25d2b8f
Scott Cardoza <scott@MacBook-Pro.localdomain>
2020-10-25 17:16:27 -0600

    add gatsby plugin to stop flash on load

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-postcss`,
+    `gatsby-plugin-styled-components`,
     // {
     //   resolve: `gatsby-plugin-purgecss`,
     //   options: {
