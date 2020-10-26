1d32b4b640d0db40ac639b5ed55d7a8f6fe20afa
Scott Cardoza <scott@MacBook-Pro.localdomain>
2020-10-25 18:21:10 -0600

    full content padding

--- src/components/PageLayouts/Full/styles.js
+++ src/components/PageLayouts/Full/styles.js
@@ -2,8 +2,7 @@ import styled from "styled-components"
 
 export const FullContainer = styled.div`
   background: #eee;
-  padding: 20px;
-  padding-top: 50px;
+  padding: 50px 20px 50px 20px
   text-align: center;
   font-size: 16px;
   line-height: 30px;
